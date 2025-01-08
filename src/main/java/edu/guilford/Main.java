94ef73e8b3e2db480bdb031a6d1e1e0121a633a4
Rob Whitnell <rwhitnel@guilford.edu>
2025-01-08 12:53:05 -0500

    updating first comment

--- src/main/java/edu/guilford/Main.java
+++ src/main/java/edu/guilford/Main.java
@@ -2,6 +2,8 @@ package edu.guilford;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");        
+        System.out.println("Hello world!");   
+        // Print out another greeting
+             
     }
 }
