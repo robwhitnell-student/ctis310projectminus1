94a17fd7048835e65cb2ceb268074ffd139ede8f
Rob Whitnell <rwhitnel@guilford.edu>
2025-02-12 09:14:12 -0500

    Updated driver program name

---
+++ src/main/java/edu/guilford/LotteryTicketDriver.java
@@ -0,0 +1,13 @@
+package edu.guilford;
+
+public class LotteryTicketDriver {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");   
+        // // create a new LotteryTicket object
+        LotteryTicket ticket = new LotteryTicket();
+
+        // print out the ticket
+        System.out.println("The ticket is " + ticket);
+             
+    }
+}
